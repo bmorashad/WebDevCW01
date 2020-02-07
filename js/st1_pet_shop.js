ad31d7f558d6f7b8f237ad0078ba7d9914661972
bsucker98 <bmorashad@gmail.com>
2020-02-07 15:03:28 +0530

    cart working js

--- js/st1_pet_shop.js
+++ js/st1_pet_shop.js
@@ -32,24 +32,83 @@ let addToCart = () => {
      <span class="td"><span class="mono">rs</span></span>
      <span class="td"><span class="cart_price pink mono">2000</span></span>
      <span class="td cart_total"><span class="cart_price pink mono">101500</span></span>
-     <span class="td cart_total"><span class="pink">TOTAL</span></span> */
+     <span class=    itmDet.forEach(el => console.log(el))
+"td cart_total"><span class="pink">TOTAL</span></span> */
     let btn = event.target
     let itm = btn.parentElement.parentElement
-    let itmImg = itm.querySelector('img').src
-    let itmDes = itm.querySelector('.item_title_p').innerHTML
-    let itmQnt = parseInt(itm.querySelector('input').value)
-    let itmCur = itm.querySelector('.currency').innerHTML.slice(0, 2).toLowerCase()
-    let itmPrice = parseInt(itm.querySelector('.itm_price').innerHTML)
-    let itmNo = document.querySelector('.cart_id_col').querySelectorAll('.tr').length + 1
-    let itmTotalPrice = itmQnt * itmPrice 
+    // let itmImg = itm.querySelector('img').src
+    // let itmDes = itm.querySelector('.item_title_p').innerHTML
+    // let itmQnt = parseInt(itm.querySelector('input').value)
+    // let itmCur = 'rs'
+    // let itmPrice = parseInt(itm.querySelector('.itm_price').innerHTML)
+    // let itmNo = document.querySelector('.cart_id_col').querySelectorAll('.td').length + 1
+    // let itmTotalPrice = itmQnt * itmPrice
 
-    
+    const itmDet = {
+            itmNo: document.querySelector('.cart_id_col').querySelectorAll('.td').length + 1,
+            itmImg: `<img src='${itm.querySelector('img').src}' class="cart_img">`,
+            itmDes: itm.querySelector('.item_title_p').innerHTML,
+            itmQnt: parseInt(itm.querySelector('input').value),
+            itmCur: 'rs',
+            itmPrice: `<span class="cart_price pink mono">${parseInt(itm.querySelector('.itm_price').innerHTML)}</span>`,
+            itmRm: '<div class="cart_remove">X</div>'
+        }
+    const itmDetArr = Object.values(itmDet)
+    const cols = document.querySelectorAll('.cart_table_col')
+    // Object.values(itmDet).forEach(val => {
+    //     let span = document.createElement('span');
+    //     span.className = 'td'
+    //     let itmTst = document.createTextNode(val)
+    //     span.appendChild(itmTst)
+    //     let colTst = cols.querySelectorAll('.td')
+    //     let lastTd = colTst[colTst.length-1]
+    //     lastTd.after(span)
+    // })
+    // cols.forEach(el => {
+    //     let span = document.createElement('span');
+    //     span.className = 'td'
+    //     let itmTst = document.createTextNode(itmDetArr[])
+    //     span.appendChild(itmTst)
+    //     let colTst = el.querySelectorAll('.td')
+    //     let lastTd = colTst[colTst.length-1]
+    //     lastTd.after(span)
+    // })
+    for(let i = 0; i < cols.length; i++) {
+        let span = document.createElement('span');
+        span.className = 'td'
+        let colTst = cols[i].querySelectorAll('.td')
+        let lastTd = colTst[colTst.length-1]
+        lastTd.after(span)
+        span.innerHTML = itmDetArr[i]
+    }
+
+    console.log(itmDet)
+    let span = document.createElement('span');
+    span.className = 'td'
+    // let itmTst = document.createTextNode(itmNo)
+    // span.appendChild(itmTst)
+    // let colTst = document.querySelector('.cart_id_col').querySelectorAll('.td')
+    // let lastTd = colTst[colTst.length-1]
+    // lastTd.after(span)
+    // let itmNoHtml = `<span class="td">${itmNo}</span>`
+    // let itmImgHtml = `<span class="td"><img src="${itmImg}" class="cart_img"></span>`
+    // let itmDesHtml = `<span class="td">${itmDes}</span>`
+    // let itmQntHtml = `<span class="td">${itmQnt}</span>`
+    // let itmCurHtml = `<span class="td">${itmCur}</span>`
+    // let itmPriceHtml = `<span class="td">${itmPrice}</span>`
+    // let itmRmHtml = '<span class="td"><div class="cart_remove">X</div></span>'
+
+
+
+
+
+    // console.log(itmImgHtml)
 
     //  console.log(itmTotalPrice)
 }
 
  // Adding EventListener
- 
+
 qntIncrease.forEach(counter => counter.addEventListener('click', qntIncrement))
 qntDecrease.forEach(counter => counter.addEventListener('click', qntDecrement))
 addItem.forEach(btn => btn.addEventListener('click', addToCart));
